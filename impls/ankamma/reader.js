fe8f31b02dff192eab46c21af6131e5291e5a758
Ankamma <ankammarao033@gmail.com>
2023-05-26 15:21:43 +0530

    step1 | Add printer mal types and reader| Ankamma

---
+++ impls/ankamma/reader.js
@@ -0,0 +1,93 @@
+const {
+  MalSymbol,
+  MalValue,
+  MalList,
+  MalVector,
+  MalNil,
+  MalBool,
+} = require("./types.js");
+
+class Reader {
+  constructor(tokens) {
+    this.tokens = tokens;
+    this.position = 0;
+  }
+
+  peek() {
+    return this.tokens[this.position];
+  }
+
+  next() {
+    const token = this.peek();
+    this.position++;
+    return token;
+  }
+}
+
+const tokenize = (str) => {
+  re = /[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"?|;.*|[^\s\[\]{}('"`,;)]*)/g;
+
+  return [...str.matchAll(re)].map((x) => x[1]).slice(0, -1);
+};
+
+const read_seq = (reader, closing) => {
+  reader.next();
+  const ast = [];
+
+  while (reader.peek() !== closing) {
+    if (reader.peek() == undefined) {
+      throw new Error("unbalanced");
+    }
+    ast.push(read_form(reader));
+  }
+
+  reader.next();
+  return ast;
+};
+
+const read_list = (reader) => {
+  const ast = read_seq(reader, ")");
+  return new MalList(ast);
+};
+
+const read_vector = (reader) => {
+  const ast = read_seq(reader, "]");
+  return new MalVector(ast);
+};
+
+const read_atom = (reader) => {
+  const token = reader.next();
+
+  if (token.match(/^-?[0-9]+$/)) {
+    return new MalValue(parseInt(token));
+  }
+
+  if (token == "true" || token == "false") {
+    return new MalBool(token);
+  }
+  
+  if (token == "nil") {
+    return new MalNil(token);
+  }
+  return new MalSymbol(token);
+};
+
+const read_form = (reader) => {
+  const token = reader.peek();
+  switch (token[0]) {
+    case "(":
+      return read_list(reader);
+    case "[":
+      return read_vector(reader);
+    default:
+      return read_atom(reader);
+  }
+};
+
+const read_str = (str) => {
+  const tokens = tokenize(str);
+  const reader = new Reader(tokens);
+  return read_form(reader);
+};
+
+module.exports = { read_str };
