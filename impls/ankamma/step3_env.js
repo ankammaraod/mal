7b3f5d597eec5098a168b7048e0688e69c45a92f
Ankamma <ankammarao033@gmail.com>
2023-05-29 17:18:12 +0530

    step3 | Add def feature | Ankamma

--- impls/ankamma/step3_env.js
+++ impls/ankamma/step3_env.js
@@ -2,14 +2,13 @@ const readline = require("readline");
 const { read_str } = require("./reader");
 const { pr_str } = require("./printer.js");
 const { MalSymbol, MalList, MalValue, MalVector } = require("./types");
-
+const { Env } = require("./env.js");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-
-const env = {
+const _env = {
   "+": (...args) => args.reduce((a, b) => a + b),
   "-": (...args) => args.reduce((a, b) => a - b),
   "*": (...args) => args.reduce((a, b) => a * b),
@@ -18,7 +17,7 @@ const env = {
 
 const eval_ast = (ast, env) => {
   if (ast instanceof MalSymbol) {
-    return env[ast.value];
+    return env.get(ast);
   }
 
   if (ast instanceof MalList) {
@@ -45,11 +44,23 @@ const EVAL = (ast, env) => {
     return ast;
   }
 
+  switch (ast.value[0].value) {
+    case "def!":
+      env.set(ast.value[1], EVAL(ast.value[2], env));
+      return env.get(ast.value[1]);
+  }
+
   const [fn, ...args] = eval_ast(ast, env).value;
   return fn.apply(null, args);
 };
 
 const PRINT = (malValue) => pr_str(malValue);
+
+const env = new Env();
+env.set(new MalSymbol("+"), (...args) => args.reduce((a, b) => a + b));
+env.set(new MalSymbol("-"), (...args) => args.reduce((a, b) => a - b));
+env.set(new MalSymbol("*"), (...args) => args.reduce((a, b) => a * b));
+env.set(new MalSymbol("/"), (...args) => args.reduce((a, b) => a / b));
 
 const rep = (str) => PRINT(EVAL(READ(str), env));
 
